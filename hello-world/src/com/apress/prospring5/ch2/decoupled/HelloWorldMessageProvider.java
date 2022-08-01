78f7e1438560ef3de463f83d961f7bbe9b6b4d3e
nohyunha95 <nohyunha95@gmail.com>
2022-08-01 23:08:31 +0900

    init commit

---
+++ hello-world/src/com/apress/prospring5/ch2/decoupled/HelloWorldMessageProvider.java
@@ -0,0 +1,9 @@
+package com.apress.prospring5.ch2.decoupled;
+
+public class HelloWorldMessageProvider implements MessageProvider {
+
+    @Override
+    public String getMessage() {
+        return "Hello World";
+    }
+}
